b73b9409596b90e5c4a667e109d68eac0d6186b5
alexey <a.lebedev.bppg@gmail.com>
2023-01-03 02:28:27 +0400

    27 CI pipeline. Автоматизация прогона тестов

--- configs/build/buildPlugins.ts
+++ configs/build/buildPlugins.ts
@@ -7,28 +7,36 @@ import { BuildOptions } from "./types/config";
 export const buildPlugins = ({
   paths,
   isDev,
-}: BuildOptions): webpack.WebpackPluginInstance[] => [
-  // генерим главный html (index.tsx) из пути src, чтоб в него встраивались скрипты
-  new htmlWebpackPlugin({
-    template: paths.html,
-  }),
-  // показывает прогресс сборки
-  new webpack.ProgressPlugin(),
-  // чтоб файлы css в сборке находились отдельно (не внутри js файла)
-  new MiniCssExtractPlugin({
-    // название на выходе
-    filename: "css/[name].[contenthash:8].css",
-    // название для чанков
-    chunkFilename: "css/[name].[contenthash:8].css",
-  }),
-  // с ним можно прокидывать глобальные переменные в само приложение
-  new DefinePlugin({
-    // называем переменные таким образом, чтоб четко понимать где переменные вебпака, а где приложения
-    __IS_DEV__: JSON.stringify(isDev), // теперь эта переменная доступна в коде (например, файл i18n.ts)
-  }),
-  // для горячей перезагрузки (чтоб при изменениях в коде не обновлять страницу)
-  // впоследствии поменяем на ReactRefreshWebpackPlugin
-  new HotModuleReplacementPlugin(),
-  // включаем плагин анализа размера пакетов
-  isDev && new BundleAnalyzerPlugin(),
-];
+}: BuildOptions): webpack.WebpackPluginInstance[] => {
+  const plugins = [
+    // генерим главный html (index.tsx) из пути src, чтоб в него встраивались скрипты
+    new htmlWebpackPlugin({
+      template: paths.html,
+    }),
+    // показывает прогресс сборки
+    new webpack.ProgressPlugin(),
+    // чтоб файлы css в сборке находились отдельно (не внутри js файла)
+    new MiniCssExtractPlugin({
+      // название на выходе
+      filename: "css/[name].[contenthash:8].css",
+      // название для чанков
+      chunkFilename: "css/[name].[contenthash:8].css",
+    }),
+    // с ним можно прокидывать глобальные переменные в само приложение
+    new DefinePlugin({
+      // называем переменные таким образом, чтоб четко понимать где переменные вебпака, а где приложения
+      __IS_DEV__: JSON.stringify(isDev), // теперь эта переменная доступна в коде (например, файл i18n.ts)
+    }),
+  ];
+
+  // данные плагины добавляем, только если не продакш сборка
+  if (isDev) {
+    // для горячей перезагрузки (чтоб при изменениях в коде не обновлять страницу)
+    // впоследствии поменяем на ReactRefreshWebpackPlugin
+    plugins.push(new HotModuleReplacementPlugin());
+    // включаем плагин анализа размера пакетов
+    plugins.push(new BundleAnalyzerPlugin());
+  }
+
+  return plugins;
+};
